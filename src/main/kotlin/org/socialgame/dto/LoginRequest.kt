5a4fbae883ff9b4deaacb6f474499f2faf618796
sito <adrianologu123@cloud.com>
2026-04-16 15:45:51 +0200

    "7 Intento arreglo 403..."

--- src/main/kotlin/org/socialgame/dto/LoginRequest.kt
+++ src/main/kotlin/org/socialgame/dto/LoginRequest.kt
@@ -1,7 +1,15 @@
 package org.socialgame.dto
 
+class LoginRequest {
+    // Getters y Setters obligatorios
+    var username: String? = null
+    var password: String? = null
 
-data class LoginRequest(
-    val username: String = "",
-    val password: String = ""
-)
+    // Constructor vacío obligatorio para Jackson
+    constructor()
+
+    constructor(username: String?, password: String?) {
+        this.username = username
+        this.password = password
+    }
+}
