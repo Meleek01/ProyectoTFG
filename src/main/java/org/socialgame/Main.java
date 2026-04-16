725484089f5635293df198c8892882e67048cd70
sito <adrianologu123@cloud.com>
2026-04-16 16:08:58 +0200

    "8 Intento arreglo 403..."

--- src/main/java/org/socialgame/Main.java
+++ src/main/java/org/socialgame/Main.java
@@ -3,7 +3,7 @@ package org.socialgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.socialgame") // <--- AÑADE ESTO
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
